f929813220b3e1832485997129d04162a650f86b
Marco Asbreuk <marco@itsasbreuk.nl>
2015-08-19 19:54:21 +0200

    added api

--- constrain.js
+++ constrain.js
@@ -1,3 +1,16 @@
+/**
+ * Plugin making moveable elements to constrain within
+ *
+ *
+ * <i>Copyright (c) 2014 ITSA - https://github.com/itsa</i>
+ * New BSD License - http://choosealicense.com/licenses/bsd-3-clause/
+ *
+ *
+ * @module constrain
+ * @class Constrain
+ * @since 0.0.1
+*/
+
 "use strict";
 module.exports = function (window) {
     require('node-plugin')(window);
